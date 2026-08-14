5b8c58b00bb1573d7afad53d9af999e44e88ab2c
Rahulonit <rahulonit@gmail.com>
2026-08-14 12:37:19 +0530

    Fix mobile visuals and connect game systems

--- android/app/src/main/java/com/flapverse/neonflight/MainActivity.java
+++ android/app/src/main/java/com/flapverse/neonflight/MainActivity.java
@@ -3,6 +3,9 @@ package com.flapverse.neonflight;
 import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
+import androidx.core.view.WindowCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.core.view.WindowInsetsControllerCompat;
 import com.getcapacitor.BridgeActivity;
 
 public class MainActivity extends BridgeActivity {
@@ -20,6 +23,13 @@ public class MainActivity extends BridgeActivity {
     }
 
     private void enterImmersiveMode() {
+        WindowCompat.setDecorFitsSystemWindows(getWindow(), false);
+        WindowInsetsControllerCompat controller =
+            WindowCompat.getInsetsController(getWindow(), getWindow().getDecorView());
+        controller.hide(WindowInsetsCompat.Type.systemBars());
+        controller.setSystemBarsBehavior(
+            WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+        );
         getWindow().getDecorView().setSystemUiVisibility(
             View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY |
             View.SYSTEM_UI_FLAG_FULLSCREEN |
